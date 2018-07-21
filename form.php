697c443f0b8191c7a5937b00dc032d8e0014be22
Griff Hall <griff@Griffs-MacBook-Pro.local>
2018-07-21 13:20:42 -0400

    switched to her email address

--- form.php
+++ form.php
@@ -8,7 +8,7 @@ $message = $_POST['message'];
 
 
 $formcontent="First Name: $fname \r\n Last Name: $lname \r\n Email: $email \r\n Subject: $subject  \r\n Message: $message";
-$recipient = "griffhall1@gmail.com";
+$recipient = "kirstensweetland@gmail.com";
 $subject = "** Coaching Website Inquiry **";
 $mailheader = "From: griff@brodcast.co \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
